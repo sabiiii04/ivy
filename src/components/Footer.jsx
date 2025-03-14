5774b4092cffead49c987f5c25f6be14c5a44669
sabina <alakbarovas@yahoo.com>
2025-03-14 14:00:57 +0400

    new updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import { FaTwitter} from 'react-icons/fa';
+import { FaFacebookF } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className="bg-[#1E5A68] text-white py-12 px-6 md:px-20">
+            <div className="max-w-[1350px] mx-auto flex flex-col md:flex-row justify-between items-start gap-10">
+                <div className="w-full md:w-1/3">
+                    <h2 className="text-[50px] font-bold">IVY</h2>
+                    <p className="text-lg uppercase tracking-wider">Healthcare Group</p>
+                    <div className="w-[80px] h-[2px] bg-white my-2"></div>
+                    <p className=" mt-2">Where the care is crafted for you, and the kindness comes from the heart.</p>
+                </div>
+                <div className="w-full md:w-1/3">
+                    <h3 className="text-lg font-semibold mb-3">Quick Links</h3>
+                    <ul className="space-y-2">
+                        <li><Link to="/" >Home</Link></li>
+                        <li><Link to="/services">Services</Link></li>
+                        <li><Link to="/amenities">Amenities</Link></li>
+                        <li><Link to="/locations">Locations</Link></li>
+                        <li><Link to="/careers">Careers</Link></li>
+                    </ul>
+                </div>
+                <div className="w-full md:w-1/3">
+                    <h3 className="text-lg font-semibold mb-3">Contact Us</h3>
+                    <p><span className="font-semibold">Phone:</span> +1 (305) 912-9260</p>
+                    <p><span className="font-semibold">Email:</span> info@ivyhealthcaregroup.com</p>
+                    <div className="mt-4 flex space-x-4text-2xl">
+                        <a href="#"><FaTwitter /></a>
+                        <a href="#"><FaFacebookF /></a>
+                        <a href="#"><FaInstagram /></a>
+                    </div>
+                </div>
+            </div>
+        </footer>
     );
 };
 
--- src/components/HomeComponents/OurBusinsessCases.jsx
+++ src/components/HomeComponents/OurBusinsessCases.jsx
@@ -19,14 +19,14 @@ const businessCases = [
 
 const BusinessCases = () => {
     return (
-        <section className="max-w-[1350px] mx-auto px-6 lg:px-20 py-16 bg-center" style={{ backgroundImage: `url(${bg4})`, }}>
-            <p className="text-[#25758A] uppercase tracking-wider text-sm">
+        <section className="max-w-[1350px] mx-auto px-6 lg:px-20 py-16 bg-center " style={{ backgroundImage: `url(${bg4})`, }}>
+            <p className="text-[#25758A] uppercase text-sm">
                 Recent Case Studies
             </p>
             <h2 className="text-4xl font-bold mt-2">OUR BUSINESS CASES</h2>
-            <div className="mt-10 grid grid-cols-1 md:grid-cols-3 gap-6">
+            <div className="mt-10 grid grid-cols-1 md:grid-cols-3 gap-6 items-center justify-center">
                 {businessCases.map((item, index) => (
-                    <div key={index} className="rounded-lg overflow-hidden shadow-lg px-20 py-30  " style={{ backgroundImage: `url(${item.img})`, }}>
+                    <div key={index} className="rounded-lg overflow-hidden shadow-lg px-20 py-30 flex justify-center" style={{ backgroundImage: `url(${item.img})`,backgroundSize: "cover", backgroundPosition: "center", height: "350px", }}>
                             <FaEye className="text-[#25758A] bg-white rounded-full p-2 size-[50px]  align-center items-center" />
                     </div>
                 ))}
